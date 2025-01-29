45b071d0527f6dc41ec0d59f7f6c9c3216e9a8c6
Edwin Garcia Quiterio <edwinegq@outlook.com>
2025-01-29 16:56:30 -0600

    CRUD completo de products

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,53 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid', { name: 'id' })
+    id: string;
+
+    @Column('text', { name: 'title', unique: true })
+    title: string;
+
+    @Column('float', { default: 0 })
+    price: number;
+
+    @Column({ type: 'text', nullable: true })
+    description: string;
+
+    @Column('text', { unique: true })
+    slug: string;
+
+    @Column('int', { default: 0 })
+    stock: number;
+
+    @Column('text', { array: true })
+    sizes: string[]
+
+    @Column('text')
+    gender: string;
+
+    // Método que se ejecuta antes de insertar un registro 
+    @BeforeInsert()
+    checkSlugInsert() {
+        if (!this.slug) {
+            this.slug = this.title;
+        }
+
+        this.slug = this.slug
+            .toLocaleLowerCase()
+            .replaceAll(' ', '_')
+            .replaceAll("'", '');
+    }
+
+    @BeforeUpdate()
+    checkSlugUpdate() {
+        this.slug = this.slug
+            .toLocaleLowerCase()
+            .replaceAll(' ', '_')
+            .replaceAll("'", '');
+    }
+
+    // @BeforeUpdate()
+    // tag
+    // images
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,97 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid'
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger = new Logger('ProductService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) { }
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto)
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+
+      this.handleDBExceptions(error);
+
+    }
+  }
+
+  async findAll({ limit = 10, offset = 0 }: PaginationDto) {
+    return await this.productRepository.find({
+      take: limit,
+      skip: offset,
+      //TODO: relations
+    });
+  }
+
+  async findOne(term: string) {
+    let product: Product;
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      // product = await this.productRepository.findOneBy({ slug: term });
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where(`LOWER(title) = :title or slug = :slug`, {
+          title: term.toLowerCase(),
+          slug: term.toLowerCase()
+        })
+        .getOne();
+    }
+
+    if (!product) {
+      throw new NotFoundException(`Product ${term} not found`);
+    }
+
+    return product;
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id: id,
+      ...updateProductDto
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product whit id ${ id } not found`);
+    }
+    try {
+      await this.productRepository.save(product)
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+
+    await this.productRepository.remove(product);
+
+    return true;
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error)
+
+    throw new InternalServerErrorException('Error')
+  }
+}
